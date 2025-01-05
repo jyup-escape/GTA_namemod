ff756cdcc10417cfb2abeff7d643da0ffba9a8a8
jyup-akarilab <jyup.ect@gmail.com>
2025-01-05 15:36:18 +0900

    Update server.lua

--- gta_mod/server.lua
+++ gta_mod/server.lua
@@ -1,12 +1,3 @@
-local arguments = {
-    { name = 'greeting', help = 'Enter a greeting message' }
-}
-
-local argsRequired = true -- 引数が必須かどうか
-
-QBCore.Commands.Add('hi', 'Send a greeting message', arguments, argsRequired, function(source, args)
-    local greeting = args[1] or 'hi'  -- 引数がなければデフォルトで 'hi' を返す
-    TriggerClientEvent('chat:addMessage', source, {
-        args = { '^1Server', greeting }
-    })
-end, 'user')
+luaCode kopierenRegisterCommand('hello', function()
+    print('Hello, welcome to my QBCore server!')
+end)
